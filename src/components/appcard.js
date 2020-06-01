e16e7d9a0148ef5816df3147ee75b0b985b32d3c
Thomas Qvidahl <thomas.qvidahl@evry.com>
2020-06-02 01:02:29 +0200

    Some fun with styling and pages.

---
+++ src/components/appcard.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from "./appcard.module.css"
+
+export default function AppCard(props) {
+
+    return (
+        <div className={styles.app}>
+            <img src={props.appIcon} className={styles.appIcon} alt="" />
+            <div className={styles.description}>
+                <h2 className={styles.appName}>{props.appName}</h2>
+                <p>{props.longDesc}</p>
+            </div>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,28 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
 import Container from "../components/container";
+import AppCard from "../components/appcard";
 
 export default function Home() {
     return (
     <Container>
         <Link to="/contact/">Contact</Link>
         <Header headerText="Apps by Qvidahl"/>
+        <AppCard
+            appName="Conference Call Bingo"
+            appIcon="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+            longDesc="Conference Call Bingo is what you play when you get a lot of conference call clichès going on..."
+            />
+        <AppCard
+            appName="Selbu-whist"
+            appIcon="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+            longDesc="Selbu-whist is a fun card game for two to five players"
+        />
+        <AppCard
+            appName="Image-generating API"
+            appIcon="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+            longDesc="This API will generate a background image for use in conference calls. Dynamic text."
+        />
     </Container>
     );
 }
