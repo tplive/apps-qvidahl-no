84d60d1a4158b6f59190baf22a5e6a21331fcf3b
Thomas Qvidahl <thomas.qvidahl@evry.com>
2020-06-01 23:39:30 +0200

    Initialised Gatsby site.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Apps by Qvidahl
+# Apps by Qvidahl
 
 This is the "official" web site for my apps. Plural, as I hope they will multiply.. :D
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home() {
+    return (
+        <div>
+        <p>Home | Contact | About</p>
+    <h1 style={{ color: `purple` }}>apps.qvidahl.no</h1>
+    </div>
+    );
+}
