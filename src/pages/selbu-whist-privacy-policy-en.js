bb95c177e46448829d345aa5ca3afda701b2afe4
Thomas Qvidahl <thomas.qvidahl@evry.com>
2020-11-29 11:44:55 +0100

    Moves Selbuwhist privacy file. Changes global background color.

--- src/pages/selbu-whist-privacy-policy-en.js
+++ src/pages/selbu-whist-privacy-policy-en.js
@@ -9,12 +9,12 @@ export default function PrivacyPolicy() {
         <Container>
             <Link to="/">Home</Link>
             <Header headerText="Selbu-whist Privacy Policy"/>
-            <p><strong>Privacy Policy</strong></p> 
+            <h2><strong>Privacy Policy</strong></h2> 
                 <p>Curium 82 Studios built the Selbu-whist app as a Commercial app. 
                     This SERVICE is provided by Curium 82 Studios and is intended 
                     for use as is.</p>
                 
-                <p>This page is used to inform visitors regarding our
+                <p>This document is used to inform users regarding our
                   policies with the collection, use, and disclosure of Personal
                   Information if anyone decided to use our Service.</p> 
                 
@@ -24,12 +24,8 @@ export default function PrivacyPolicy() {
                   used for providing and improving the Service. 
                   We will not use or share your information with
                   anyone except as described in this Privacy Policy.</p> 
-                <p>
-                  The terms used in this Privacy Policy have the same meanings
-                  as in our Terms and Conditions, which is accessible at
-                  Selbu-whist unless otherwise defined in this Privacy Policy.
-                </p> 
-                <p><strong>Information Collection and Use</strong></p> 
+                
+                <h3><strong>Information Collection and Use</strong></h3> 
                 <p>
                   For a better experience, while using our Service, we
                   may require you to provide us with certain personally
@@ -46,12 +42,12 @@ export default function PrivacyPolicy() {
                     by the app:
                   </p> 
                     <ul>
-                      <li><a href="https://www.google.com/policies/privacy/" target="_blank">Google Play Services</a></li>
-                      <li><a href="https://firebase.google.com/policies/analytics" target="_blank">Google Analytics for Firebase</a></li>
-                      <li><a href="https://www.facebook.com/about/privacy/update/printable" target="_blank">Facebook</a></li>
+                      <li><a href="https://www.google.com/policies/privacy/" target="_blank" rel="noreferrer">Google Play Services</a></li>
+                      
+                      <li><a href="https://www.facebook.com/about/privacy/update/printable" target="_blank" rel="noreferrer">Facebook</a></li>
                     </ul></div> 
                     
-                <p><strong>Log Data</strong></p> <p>
+                <h4><strong>Log Data</strong></h4> <p>
                   we want to inform you that whenever you
                   use our Service, in a case of an error in the app
                   we collect data and information (through third party
@@ -61,7 +57,7 @@ export default function PrivacyPolicy() {
                   configuration of the app when utilizing our Service,
                   the time and date of your use of the Service, and other
                   statistics.
-                </p> <p><strong>Cookies</strong></p> <p>
+                </p> <h4><strong>Cookies</strong></h4> <p>
                   Cookies are files with a small amount of data that are
                   commonly used as anonymous unique identifiers. These are sent
                   to your browser from the websites that you visit and are
@@ -74,7 +70,7 @@ export default function PrivacyPolicy() {
                   and know when a cookie is being sent to your device. If you
                   choose to refuse our cookies, you may not be able to use some
                   portions of this Service.
-                </p> <p><strong>Service Providers</strong></p> <p>
+                </p> <h4><strong>Service Providers</strong></h4> <p>
                   we may employ third-party companies and
                   individuals due to the following reasons:
                 </p> <ul><li>To facilitate our Service;</li> <li>To provide the Service on our behalf;</li> <li>To perform Service-related services; or</li> <li>To assist us in analyzing how our Service is used.</li></ul> <p>
@@ -83,14 +79,14 @@ export default function PrivacyPolicy() {
                   Information. The reason is to perform the tasks assigned to
                   them on our behalf. However, they are obligated not to
                   disclose or use the information for any other purpose.
-                </p> <p><strong>Security</strong></p> <p>
+                </p> <h4><strong>Security</strong></h4> <p>
                   we value your trust in providing us your
                   Personal Information, thus we are striving to use commercially
                   acceptable means of protecting it. But remember that no method
                   of transmission over the internet, or method of electronic
                   storage is 100% secure and reliable, and we cannot
                   guarantee its absolute security.
-                </p> <p><strong>Links to Other Sites</strong></p> <p>
+                </p> <h4><strong>Links to Other Sites</strong></h4> <p>
                   This Service may contain links to other sites. If you click on
                   a third-party link, you will be directed to that site. Note
                   that these external sites are not operated by us.
@@ -99,7 +95,7 @@ export default function PrivacyPolicy() {
                   no control over and assume no responsibility for the content,
                   privacy policies, or practices of any third-party sites or
                   services.
-                </p> <p><strong>Children’s Privacy</strong></p> <p>
+                </p> <h4><strong>Children’s Privacy</strong></h4> <p>
                   These Services do not address anyone under the age of 13.
                   we do not knowingly collect personally
                   identifiable information from children under 13. In the case
@@ -109,13 +105,13 @@ export default function PrivacyPolicy() {
                   and you are aware that your child has provided us with
                   personal information, please contact us so that
                   we will be able to do necessary actions.
-                </p> <p><strong>Changes to This Privacy Policy</strong></p> <p>
+                </p> <h4><strong>Changes to This Privacy Policy</strong></h4> <p>
                   we may update our Privacy Policy from
                   time to time. Thus, you are advised to review this page
                   periodically for any changes. we will
                   notify you of any changes by posting the new Privacy Policy on
                   this page.
-                </p> <p>This policy is effective as of 2020-06-01</p> <p><strong>Contact Us</strong></p> <p>
+                </p> <p>This policy is effective as of 2020-06-01</p> <h4><strong>Contact Us</strong></h4> <p>
                   If you have any questions or suggestions about our
                   Privacy Policy, do not hesitate to contact us at uumuus@gmail.com.
                 </p> 
