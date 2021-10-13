61c85f9f223bf1b6b34e30a1f551c460635fe30b
Thomas Qvidahl <thomas.qvidahl@tietoevry.com>
2021-10-13 09:05:06 +0200

    🐛 Fix CSS imports in components

--- src/components/container.js
+++ src/components/container.js
@@ -1,8 +1,8 @@
 import React from "react"
-import containerStyles from "./container.module.css"
+import * as containerStyles from "./container.module.css"
 
 export default function Container({ children }) {
     return (
-    <div className={containerStyles.container}>{ children }</div>
+        <div className={containerStyles.container}>{children}</div>
     )
-} 
+}
